b22c6b52dfbca155b3db9f56c91e6a46b8a35859
Ramana Kumar <ramana@member.fsf.org>
2023-06-07 08:50:48 +0100

    Add another test

--- epineph/test/Contract.t.sol
+++ epineph/test/Contract.t.sol
@@ -6,10 +6,12 @@ import "forge-std/Test.sol";
 
 contract ContractTest is Test {
     Contract public c;
-    event Deposit(address indexed, uint256, uint256, uint256);
     event Transfer(address indexed, address indexed, uint256);
+    event Deposit(address indexed, uint256, uint256, uint256);
+    event Drain(address indexed, uint256, uint256);
     address rETHAddress = 0xae78736Cd615f374D3085123A210448E74Fc6393;
     address stETHAddress = 0xae7ab96520DE3A18E5e111B5EaAb095312D7fE84;
+    address ownerAddress = 0x1234567890000000000000000000000000000000;
 
     function setUp() public {
         vm.createSelectFork(vm.envString("MAINNET_RPC_URL"));
@@ -23,26 +25,34 @@ contract ContractTest is Test {
 
     function testDeposit() public {
         // pretend to be wstETH (which has a lot of stETH) since deal seems to break on lido
-        address wstETH = 0x7f39C581F595B53c5cb19bD0b3f8dA6c935E2Ca0;
-        vm.startPrank(wstETH);
+        address wstETHAddress = 0x7f39C581F595B53c5cb19bD0b3f8dA6c935E2Ca0;
+        vm.startPrank(wstETHAddress);
         // deal(stETHAddress, address(this), 42); // give ourselves some stETH
         deal(rETHAddress, address(c), 42); // give contract some rETH
         ERC20(stETHAddress).approve(address(c), 24);
         uint256 expected = rETH(rETHAddress).getRethValue(24);
         vm.expectEmit(true, true, true, true, stETHAddress);
-        emit Transfer(wstETH, address(c), 24);
+        emit Transfer(wstETHAddress, address(c), 24);
         vm.expectEmit(true, true, true, true, rETHAddress);
-        emit Transfer(address(c), wstETH, expected);
+        emit Transfer(address(c), wstETHAddress, expected);
         vm.expectEmit(true, true, true, true, address(c));
-        emit Deposit(wstETH, 0, expected, 24);
+        emit Deposit(wstETHAddress, 0, expected, 24);
         c.deposit(24);
         vm.stopPrank();
     }
 
+    function testDrainNothing() public {
+        vm.expectEmit(true, true, true, true, stETHAddress);
+        emit Transfer(address(c), ownerAddress, 0);
+        vm.expectEmit(true, true, true, true, address(c));
+        emit Drain(address(this), 0, 0);
+        c.drain();
+    }
+
     function testDepositNoReth() public {
         // pretend to be wstETH (which has a lot of stETH) since deal seems to break on lido
-        address wstETH = 0x7f39C581F595B53c5cb19bD0b3f8dA6c935E2Ca0;
-        vm.startPrank(wstETH);
+        address wstETHAddress = 0x7f39C581F595B53c5cb19bD0b3f8dA6c935E2Ca0;
+        vm.startPrank(wstETHAddress);
         ERC20(stETHAddress).approve(address(c), 24);
         vm.expectRevert();
         c.deposit(12);
@@ -75,14 +85,12 @@ contract ContractTest is Test {
 interface Contract {
     function deposit(uint256) payable external;
     function drain() external;
-    event Deposit(address indexed, uint256, uint256, uint256);
-    event Drain(address indexed, uint256, uint256);
 }
 
 interface rETH {
-  function getRethValue(uint256) view external returns (uint256);
+    function getRethValue(uint256) view external returns (uint256);
 }
 
 interface ERC20 {
-  function approve(address, uint256) external returns (bool);
+    function approve(address, uint256) external returns (bool);
 }
